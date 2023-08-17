13643bbd677d2c8838abd515bb6d4090a54c0b06
PariKia <kiaeifar.pardis@gmail.com>
2023-08-18 02:01:41 +0330

    Add files via upload

---
+++ PhoneInfoExtractor.py
@@ -0,0 +1,13 @@
+import phonenumbers
+from phonenumbers import geocoder, carrier
+
+# The phone number you want to analyze
+phone_number = "+989123456789" 
+
+# Parsing the phone number for geolocation (CH)
+ch_number = phonenumbers.parse(phone_number, "CH")
+print("Geolocation:", geocoder.description_for_number(ch_number, 'en'))
+
+# Parsing the phone number for carrier information (RO)
+service_number = phonenumbers.parse(phone_number, 'RO')
+print("Carrier:", carrier.name_for_number(service_number, 'en'))
